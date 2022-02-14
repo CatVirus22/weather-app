dd016f8ff669acbb04aef21af313267ea5d66851
Marizabelle Aldana <94816917+CatVirus22@users.noreply.github.com>
2022-02-14 00:08:41 +0000

    changes CSS, HTML and JS
    integrated API function for daily forecast
    deleted second row showing hourly forecast.
    changess made to CSS

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ function displayTemp(response) {
   tempElement.innerHTML = `${cityTemp}`;
 
   celsiusTemp = response.data.main.temp;
+  apiForecast(response.data.coord);
 }
 
 function currentTemp(response) {
@@ -27,6 +28,7 @@ function currentTemp(response) {
   cityElement.innerHTML = `${displayCity}`;
 
   celsiusTemp = response.data.main.temp;
+  apiForecast(response.data.coord);
 }
 
 function dateInfo(timestamp) {
@@ -78,6 +80,57 @@ function timeInfo(timestamp) {
   }
 
   return `Last time updated: ${currentHour}:${currentMinutes}`;
+}
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let weekDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return weekDay[day];
+}
+
+function displayForecast(response) {
+  let dailyTemps = response.data.daily;
+  console.log(response.data);
+  let forecastElement = document.querySelector("#daily-forecast-temps");
+
+  let forecastHTML = `<div class="d-flex id="daily-forecast-temps">`;
+
+  dailyTemps.forEach(function (day, index) {
+    if (index < 7) {
+      forecastHTML =
+        forecastHTML +
+        `
+                  <div class="flex-column" id="daily-forecast-temps">
+                     <div class="col">
+                     <span id="highest"><strong>${Math.round(
+                       day.temp.max
+                     )}°</strong></span>
+                     <span><strong> - </strong></span>
+                     <span id="lowest"><strong>${Math.round(
+                       day.temp.min
+                     )}°</strong></span>
+                    </div>
+                    <img src="http://openweathermap.org/img/wn/${
+                      day.weather[0].icon
+                    }@2x.png"/>
+                    <p class="mb-0"><strong>${formatDate(day.dt)}</strong></p>
+                  </div>
+                  </div>
+    `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function apiForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "d67292210b7875b5cf04663144f38fa9";
+  let forecastApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(forecastApi);
+  axios.get(forecastApi).then(displayForecast);
 }
 
 function currentForecast(response) {
@@ -134,5 +187,3 @@ fahrenheit.addEventListener("click", displayFtemp);
 
 let celsius = document.querySelector("#temp-celsius");
 celsius.addEventListener("click", displayCtemp);
-
-celsiusTemp = null;
